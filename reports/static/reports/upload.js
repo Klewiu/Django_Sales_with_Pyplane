145763961a2fc017e911a3d785656ee0f1334190
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-07-18 12:29:28 +0200

    Add alert do dropzone

--- reports/static/reports/upload.js
+++ reports/static/reports/upload.js
@@ -1,4 +1,13 @@
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+const alertBox = document.getElementById('alert-box')
+
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
 
 Dropzone.autoDiscover = false
 const myDropzone = new Dropzone('#my-dropzone', {
@@ -7,6 +16,14 @@ const myDropzone = new Dropzone('#my-dropzone', {
         this.on('sending', function(file, xhr, formData) {
             console.log('sending')
             formData.append('csrfmiddlewaretoken', csrf)
+        })
+        this.on('success',function(file, response){
+            const ex = response.ex
+            if (ex) {
+                handleAlerts('danger','File already exists')
+            } else {
+                handleAlerts('success','Your file has been uploaded')
+            }
         })
     },
     maxFiles: 3,
--- reports/views.py
+++ reports/views.py
@@ -23,39 +23,45 @@ class UploadTemplateView(TemplateView):
 def csv_upload_view(request):
     print('File is being send')
     if request.method == "POST":
+        csv_file_name = request.FILES.get('file').name
         csv_file = request.FILES.get('file')
-        obj = CSV.objects.create(file_name = csv_file)
+        obj, created= CSV.objects.get_or_create(file_name = csv_file_name)
 
-        with open(obj.file_name.path, 'r') as f:
-            reader = csv.reader(f)
-            reader.__next__()
-            for row in reader:
-                data = "".join(row)
-                data = data.split(';')
-                # data.pop()
+        if created:
+            obj.csv_file = csv_file
+            obj.save()
+            with open(obj.csv_file.path, 'r') as f:
+                reader = csv.reader(f)
+                reader.__next__()
+                for row in reader:
+                    data = "".join(row)
+                    data = data.split(';')
+                    # data.pop()
 
-                transaction_id = data[1]
-                product = data[2]
-                quantity = int(data[3])
-                customer = data[4]
-                date = parse_date(data[5])
-                print(date)
+                    transaction_id = data[1]
+                    product = data[2]
+                    quantity = int(data[3])
+                    customer = data[4]
+                    date = parse_date(data[5])
+                    print(date)
 
-                try:
-                    product_obj = Product.objects.get(name__iexact=product)
-                except Product.DoesNotExist:
-                    product_obj = None
+                    try:
+                        product_obj = Product.objects.get(name__iexact=product)
+                    except Product.DoesNotExist:
+                        product_obj = None
 
-                if product_obj is not None:
-                    customer_obj, _ = Customer.objects.get_or_create(name=customer)
-                    salesman_obj = Profile.objects.get(user=request.user)
-                    position_obj = Position.objects.create(product=product_obj, quantity=quantity, created=date)
+                    if product_obj is not None:
+                        customer_obj, _ = Customer.objects.get_or_create(name=customer)
+                        salesman_obj = Profile.objects.get(user=request.user)
+                        position_obj = Position.objects.create(product=product_obj, quantity=quantity, created=date)
 
-                    sale_obj, _ = Sale.objects.get_or_create(transaction_id=transaction_id, customer=customer_obj,
-                                                             salesman=salesman_obj, created=date)
-                    sale_obj.positions.add(position_obj)
-                    sale_obj.save()
-
+                        sale_obj, _ = Sale.objects.get_or_create(transaction_id=transaction_id, customer=customer_obj,
+                                                                 salesman=salesman_obj, created=date)
+                        sale_obj.positions.add(position_obj)
+                        sale_obj.save()
+                return JsonResponse({'ex': False})
+        else:
+            return JsonResponse({'ex': True})
 
     return HttpResponse()
 
